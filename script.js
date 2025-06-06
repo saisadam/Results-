fb609096c6c54af6b7a73c4ca0c0c9d9c2055b53
saisadam <creatorsaino1@gmail.com>
2025-06-07 04:01:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
 const students = {
-  "1001": { name: "Ravi Kumar", marks: 85, grade: "A" },
-  "1002": { name: "Anjali Sharma", marks: 74, grade: "B" },
-  "1003": { name: "Mohit Reddy", marks: 56, grade: "C" },
+  "23F91A05G5": { name: "Shaik Rahamthulla", result : "all pass", grade: "A" },
+"23F91A05I3": { name: "Sai Sadam", result : "all pass", grade: "A" },
+  "23F91A05F1": { name: "Thomas spark", result : "all pass", grade: "A" },
+"23F91A05H8": { name: "raavan ka big fan", result : "all pass", grade: "A" },
+
+"23F91A05H2": { name: "Davood bhai ERRI💐", result : "all pass", grade: "A" },
+"23F91A05I5": { name: "Siva Reddy", result : "all pass", grade: "A" },
+"23F91A05J3": { name: "Shesi", result : "all pass", grade: "A" },
+"23F91A05J5": { name: "THARUN", result : "all pass", grade: "A" },
+
+
+"23F91A05F6": { name: "ABDUL", result : "all pass", grade: "A" },
+"23F91A05F8": { name: "SANDEEP LOVES D❤️", result : "all pass", grade: "A" },
+
+
+
+  "23F91A05F9": { name: "NAVEEN lover boy", result : "all pass", grade: "A" },
+"23F91A05H3": { name: "blade babji ", result : "all pass", grade: "A" },
+
+
 };
 
 function getResult() {
